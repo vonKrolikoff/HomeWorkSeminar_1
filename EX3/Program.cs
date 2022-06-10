a7ed462c94c8bcd4e128e16efa09cbadda57387c
von_Krolikoff <milkbrother@yandex.ru>
2022-06-10 21:23:43 +0300

    Записана 3-я задача

---
+++ EX3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Проверка четности числа:");
+
+Console.WriteLine("Введите числo - ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является четным");
+}
+else
+{
+    Console.WriteLine("Число " + number + " является нечетным");
+}
