82d11cbf5c68787e4c0d948df8a35cf28096427c
von_Krolikoff <milkbrother@yandex.ru>
2022-06-10 20:58:24 +0300

    Записана 1-я задача

---
+++ EX1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое числo - ");
+string input = Console.ReadLine();
+int number1 = Convert.ToInt32(input);
+
+Console.WriteLine("Введите второе числo -");
+string input1 = Console.ReadLine();
+int number2 = Convert.ToInt32(input1);
+
+if (number1 == number2)
+{
+    Console.WriteLine (number1 + " = " + number2);
+}
+
+if (number1 > number2)
+{
+    Console.WriteLine (number1 + " > " + number2);
+}
+if (number1 < number2)
+{
+    Console.WriteLine (number1 + " < " + number2);
+}    
