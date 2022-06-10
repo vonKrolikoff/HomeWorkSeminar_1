5852b80ee88f3abb3b0ab6e20551faf76a66a0cc
von_Krolikoff <milkbrother@yandex.ru>
2022-06-10 21:52:11 +0300

    Final version

---
+++ EX4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Находим все чётные числа от 1 до N:");
+
+Console.WriteLine("Введите числo N - ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int res = 2;
+
+while (res < number)
+{
+    Console.Write(res + " ");
+    res = res + 2;
+}
